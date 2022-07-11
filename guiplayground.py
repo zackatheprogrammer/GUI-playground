c09c5ef10f9173435a01e294933b3978f695380d
zackatheprogrammer <109054768+zackatheprogrammer@users.noreply.github.com>
2022-07-11 11:01:28 +0200

    Update guiplayground.py

--- guiplayground.py
+++ guiplayground.py
@@ -12,6 +12,6 @@ welcome_message = Text(app, text="Welcome to my app", size=40, font="Times new r
 my_name = TextBox(app, width=30)
 update_text = PushButton(app, command=say_my_name, text="Display my name")
 text_size = Slider(app, command=change_text_size, start=10, end=80)
-my_cat = Picture(app, image="cat.gif")
+
 
 app.display()
